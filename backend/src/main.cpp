cbe32f10993f35271c092845ddcf96b175765dfb
reebuker <spontofs@gmail.com>
2026-03-28 21:10:58 +0300

    bebra

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -5,10 +5,17 @@ int main()
     crow::SimpleApp app;
 
 
+    // CROW_ROUTE(app, "/")
+    // ([](){
+    //     crow::response res;
+    //     res.set_static_file_info("")
+    // });
+
     CROW_ROUTE(app, "/add/<int>/<int>/") 
     ([](int a, int b){
         return a + b;
     });
+
 
     app.port(3000).multithreaded().run();
 }
